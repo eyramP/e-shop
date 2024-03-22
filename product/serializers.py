e245f2f680bc1a6ab939ddd0b01456f47b18d1ad
Eyram Cobblah <ecobblah2@gmail.com>
2024-03-22 04:27:39 +0000

    Create view to fetch all products and serializer to serializer to serialize the data

---
+++ product/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import Product
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = [
+            "id",
+            "name",
+            "description",
+            "price",
+            "stock",
+            "category",
+            "brand",
+            "ratings",
+            "user",
+            "created_at",
+        ]
--- product/views.py
+++ product/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from .models import Product
+from .serializers import ProductSerializer
+
+@api_view(["GET"])
+def get_products(request):
+    products = Product.objects.all()
+
+    serializer = ProductSerializer(products, many=True)
+    return Response({"products": serializer.data})
