865ff06d8d2a8d5ee5649a6bd554bd8bc6bd54ed
Eyram Cobblah <ecobblah2@gmail.com>
2024-03-22 17:20:19 +0000

    Add search functionalities to the products endpoint

--- product/filters.py
+++ product/filters.py
@@ -3,11 +3,16 @@ from .models import Product
 
 
 class ProductFilter(filters.FilterSet):
+    keyword = filters.CharFilter(field_name="name", lookup_expr="icontains")
+    min_price = filters.NumberFilter(field_name="price" or 0, lookup_expr="gte")
+    max_price = filters.NumberFilter(field_name="price"or 10000, lookup_expr="lte")
+
     class Meta:
         model = Product
         fields = [
+            "keyword",
             "category",
             "brand",
-            "price"
+            "min_price",
+            "max_price",
         ]
-
