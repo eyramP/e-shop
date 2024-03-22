f3963968e352ff94a0da52c7b0e8621cdbc1b0a9
Eyram Cobblah <ecobblah2@gmail.com>
2024-03-22 04:53:23 +0000

    Creaet a view for getting product details

--- product/urls.py
+++ product/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("products/", views.get_products, name="get_products")
+    path("products/", views.get_products, name="get_products"),
+    path("products/<str:id>/", views.get_product_details, name="get_products_details"),
 ]
--- product/views.py
+++ product/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
@@ -11,3 +12,12 @@ def get_products(request):
 
     serializer = ProductSerializer(products, many=True)
     return Response({"products": serializer.data})
+
+
+@api_view(["GET"])
+def get_product_details(request, id):
+    product = get_object_or_404(Product, id=id)
+
+    serializer = ProductSerializer(product)
+
+    return Response({"product": serializer.data})
