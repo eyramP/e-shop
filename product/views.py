583d55398ae47e539ea1f6c1cb7e5385fa5ee1c8
Eyram Cobblah <ecobblah2@gmail.com>
2024-03-22 18:01:12 +0000

    Implement pagination for products endpoint

--- product/views.py
+++ product/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.pagination import PageNumberPagination
 
 from .models import Product
 from .serializers import ProductSerializer
@@ -11,9 +12,23 @@ from .filters import ProductFilter
 @api_view(["GET"])
 def get_products(request):
     filterset = ProductFilter(request.GET, queryset=Product.objects.all().order_by("id"))
+    count = filterset.qs.count()
 
-    serializer = ProductSerializer(filterset.qs, many=True)
-    return Response({"products": serializer.data})
+    #pagination
+    resPerPage = 5
+    paginator = PageNumberPagination()
+    paginator.page_size = resPerPage
+
+    queryset = paginator.paginate_queryset(filterset.qs, request)
+
+    serializer = ProductSerializer(queryset, many=True)
+    return Response(
+        {
+            "count": count,
+            "restPerPage": resPerPage,
+            "products": serializer.data
+         }
+        )
 
 
 @api_view(["GET"])
